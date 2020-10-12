62199c99321a0ef8dc6213d1f6dd6cb20b898963
erikleisinger <erikleisinger@gmail.com>
2020-10-12 20:52:55 +0000

    added assignment_submissions and assignments tables

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,14 @@
+CREATE TABLE assignments (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(255),
+content TEXT,
+day INTEGER,
+chapter INTEGER,
+duration INTEGER);
+
+CREATE TABLE assingment_submissions (
+id SERIAL PRIMARY KEY NOT NULL,
+assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+duration INTEGER,
+submission_date DATE);
