8439daaf45d55ae771208343d2ae7b3b3b734e84
erikleisinger <erikleisinger@gmail.com>
2020-10-12 22:15:51 +0000

    exercises part 3: practicing GROUP BY, HAVING, and aggregate functions

---
+++ 3_queries_group-by/low_avg_completion.sql
@@ -0,0 +1,8 @@
+SELECT students.name, AVG(assignment_submissions.duration) AS average_assignment_duration, AVG(assignments.duration) AS average_estimated_duration
+FROM assignments
+JOIN assignment_submissions ON assignment_submissions.assignment_id = assignments.id
+JOIN students ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_assignment_duration;
